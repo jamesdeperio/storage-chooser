1834036f73035827c24c2431c1e9ae6f5738679d
jamesdeperio <deperiojames20@gmail.com>
2018-04-02 17:19:07 +0800

    Update ChooserDialogFragment.java

--- storagechooser/src/main/java/com/codekidlabs/storagechooser/fragments/ChooserDialogFragment.java
+++ storagechooser/src/main/java/com/codekidlabs/storagechooser/fragments/ChooserDialogFragment.java
@@ -261,7 +261,7 @@ public class ChooserDialogFragment extends android.app.DialogFragment {
             if (!f.getName().equals(MemoryUtil.SELF_DIR_NAME)
                     && !f.getName().equals(MemoryUtil.EMULATED_DIR_KNOX)
                     && !f.getName().equals(MemoryUtil.EMULATED_DIR_NAME)
-                    && !f.getName().equals(MemoryUtil.SDCARD0_DIR_NAME)
+                 //   && !f.getName().equals(MemoryUtil.SDCARD0_DIR_NAME)
                     && !f.getName().equals(MemoryUtil.CONTAINER)
                     && !f.getName().toLowerCase().contains("enc_emulated")) {
                 Storages sharedStorage = new Storages();
